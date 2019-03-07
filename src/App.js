34595ed907a6abbfde0c68929c0ab171e06fd4e6
Dantiff <20873103+Dantiff@users.noreply.github.com>
2019-03-07 20:31:58 +0300

    corectly structure with styled side nav

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React, {Component} from 'react'
 import {Redirect, Switch, Route} from 'react-router'
 import styled from 'styled-components'
+import {NavbarBrand, Nav, NavItem, NavLink} from 'reactstrap'
 import {TestAlert, TestBadge, TestButton} from './components'
 
 const StyledApp = styled.div`
   display: flex;
   text-align: center;
   min-height: 100vh;
+  a {
+    text-decoration: none;
+  }
 `
 const StyledSideNav = styled.div`
   width: 20%;
   background-color: #282c34;
+  text-align: left;
+  .navbar-brand {
+    font-weight: bold;
+    font-size: 2rem;
+    color: white;
+  }
+  .navbar-nav {
+    list-style: none;
+  }
 `
 const StyledAppColumn = styled.div`
   flex-grow: 1;
@@ -29,11 +42,33 @@ const Main = styled.main`
   position: relative;
   min-height: calc(100vh - 138px);
 `
+const StyledNavItem = styled(NavItem)`
+  padding: 12px 8px;
+  a {
+    color: wheat;
+  }
+`
 
 const SideNav = ({...props}) => {
   return (
     <StyledApp>
-      <StyledSideNav>Side Nav</StyledSideNav>
+      <StyledSideNav>
+        <Nav className="ml-auto" navbar>
+          <NavbarBrand href="/">Cast UI</NavbarBrand>
+          <StyledNavItem>
+            <NavLink href="/home">Home</NavLink>
+          </StyledNavItem>
+          <StyledNavItem>
+            <NavLink href="/alert">Alert</NavLink>
+          </StyledNavItem>
+          <StyledNavItem>
+            <NavLink href="/Badge">Badge</NavLink>
+          </StyledNavItem>
+          <StyledNavItem>
+            <NavLink href="/Button">Button</NavLink>
+          </StyledNavItem>
+        </Nav>
+      </StyledSideNav>
       <StyledAppColumn>{props.children}</StyledAppColumn>
     </StyledApp>
   )
